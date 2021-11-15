ce5d006b955ed56294d45f277490d5097c64c96e
Maylor David Bustamante Mercado <maylorbustamante2001@gmail.com>
2021-11-15 12:21:40 +0100

    Tengo la funsionalidad de sumar

--- IncredibleCalculator.java
+++ IncredibleCalculator.java
@@ -6,26 +6,12 @@ public class IncredibleCalculator {
 
         System.out.println("Selecciona la operación que prefieras realizar: ");
         System.out.println("1. Sumar");
-        System.out.println("2. Restar");
-        System.out.println("3. Multiplicar");
-        System.out.println("4. Dividir (división entera)");
-        System.out.println("5. Dividir (división con decimales)");
-
         int opcion = leerEnteroEntrada();
 
 
         switch (opcion) {
             case 1: //Sumar. Debes leer dos numeros enteros de entrada e imprimir su suma
                     break;
-            case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
-                    break;
-            case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
-                    break;
-            case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
-                    break;
-            case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
-                    break;
-
         }
 
     }
@@ -33,23 +19,6 @@ public class IncredibleCalculator {
     public static int suma(int a, int b) {
         return a + b;
     }
-
-    public static int resta(int a, int b) {
-        return a - b;
-    }
-
-    public static int multiplicacion(int a, int b) {
-        return a * b;
-    }
-
-    public static int divisionEntera(int a, int b) {
-        return a / b;
-    }
-
-    public static double divisionConDecimales(int a, int b) {
-        return a / (b * 1.0);
-    }
-
     public static int leerEnteroEntrada() {
         try {
             Scanner sc = new Scanner(System.in);
